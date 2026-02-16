3e33c86418b657f32f8badbe8361aa83e18bff4a
jake <hello@jake101.com>
2026-02-16 01:34:45 -0800

    fix: remove stray backtick from 70mm feed URL in playground config

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   podcast: {
-    feedUrl: 'https://podnews.net/rss',
-    siteTitle: 'Podnews',
+    feedUrl: 'https://feeds.transistor.fm/70mm',
+    siteTitle: '70mm Pod',
     platforms: {
       apple: 'https://podcasts.apple.com/podcast/id1488993527',
       rss: 'https://podnews.net/rss',
