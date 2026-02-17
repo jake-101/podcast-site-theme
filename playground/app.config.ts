6efac420cf08e1b942a5db5b911d3db4cd87ef1a
jake <hello@jake101.com>
2026-02-16 20:34:37 -0800

    chore: switch playground to The Changelog feed

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -1,14 +1,20 @@
 export default defineAppConfig({
   podcast: {
-    feedUrl: 'https://feeds.transistor.fm/70mm',
-    siteTitle: '70mm',
+    feedUrl: 'https://changelog.com/podcast/feed',
+    siteTitle: 'The Changelog',
     platforms: {
-      rss: 'https://feeds.transistor.fm/70mm',
+      rss: 'https://changelog.com/podcast/feed',
     },
     episodesPerPage: 18,
     heroType: 'featured',
     navLogo: 'image',
     theme: 'auto',
+    newsletter: {
+      platform: 'substack',
+      url: 'https://changelog.substack.com',
+      label: 'Get new episodes in your inbox',
+      description: 'Subscribe for weekly software developer news.',
+    },
   },
 })
 
